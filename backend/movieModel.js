bc439da2da073b487665830ed410d60d470c64e6
RajbharSaheb <110810360+RajbharSaheb@users.noreply.github.com>
2025-07-14 22:47:54 +0530

    Delete backend directory

--- backend/movieModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-// Define schema based on your existing collection's structure
-const movieSchema = new mongoose.Schema({}, { strict: false });
-
-// Use the exact collection name
-module.exports = mongoose.model('RajbharPahadan', movieSchema, 'RajbharPahadan');
-//                ^ model name         ^ schema      ^ actual collection name
